a4ca5bfa265c735a49e369d008c81674f9af563a
nickmurr <nickmur72@gmail.com>
2021-12-16 16:06:28 +0200

    debug v2

--- cmd/app/telegram-pipe.go
+++ cmd/app/telegram-pipe.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"fmt"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go.uber.org/zap"
 	"test/internal/telegram"
@@ -12,13 +10,10 @@ func (a *App) runTelegramPipeline() {
 	a.bot.Debug = true
 	updates := a.bot.ListenForWebhook("/" + a.bot.Token)
 
-	for {
-		select {
-		case <-a.ctx.Done():
-			a.logger.Info("Finished telegram pipeline")
-			return
-		case update := <-updates:
-			fmt.Println(update.Message.Text)
+	for update := range updates {
+		if update.Message != nil {
+			a.logger.Info("message", zap.String("username", update.Message.From.UserName), zap.String("text", update.Message.Text))
+
 			switch update.Message.Command() {
 			case "good_mornig_radnyli":
 				_, err := a.bot.Send(telegram.Nice(update.Message.Chat.ID, telegram.RandomGreetings(update.Message.From.UserName)))
@@ -46,7 +41,47 @@ func (a *App) runTelegramPipeline() {
 				}
 				break
 			}
+		} else {
+			a.logger.Info("message is empty", zap.String("user", update.Message.From.UserName))
 		}
 	}
+
+	//
+	// for {
+	// 	select {
+	// 	case <-a.ctx.Done():
+	// 		a.logger.Info("Finished telegram pipeline")
+	// 		return
+	// 	case update := <-updates:
+	// 		fmt.Println(update.Message.Text)
+	// 		switch update.Message.Command() {
+	// 		case "good_mornig_radnyli":
+	// 			_, err := a.bot.Send(telegram.Nice(update.Message.Chat.ID, telegram.RandomGreetings(update.Message.From.UserName)))
+	// 			if err != nil {
+	// 				a.logger.Error("couldn't send message", zap.Error(err))
+	// 				break
+	// 			}
+	//
+	// 		case "good_morning_pidarasi":
+	// 			_, err := a.bot.Send(telegram.Nice(update.Message.Chat.ID, "ДОБРОЕ УТРО ГЕЁЧКИ!"))
+	// 			if err != nil {
+	// 				a.logger.Error("couldn't send message", zap.Error(err))
+	// 			}
+	//
+	// 			break
+	// 		case "puss_deep":
+	// 			telegram.NewPuss(a.bot, update.Message.Chat.ID, update.Message.From.UserName, a.config.TelegramApiKey)
+	// 			break
+	//
+	// 		default:
+	// 			reply := "Я НЕ ПОНИМАЮ ЧТО ПРОИСХОДИТ !"
+	// 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+	// 			if _, err := a.bot.Send(msg); err != nil {
+	// 				a.logger.Error("default error sending message", zap.Error(err))
+	// 			}
+	// 			break
+	// 		}
+	// 	}
+	// }
 
 }
