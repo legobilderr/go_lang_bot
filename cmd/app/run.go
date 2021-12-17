00a6c4e0a22f7900fd40156ad839ebd998913a7c
Oleg <olegpolechko123@gmail.com>
2021-12-17 16:08:22 +0200

    simple refactor 3

--- cmd/app/run.go
+++ cmd/app/run.go
@@ -2,47 +2,23 @@ package app
 
 import (
 	"context"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
+	"expvar"
+	"test/internal/config"
 )
 
 func Run() {
 	ctx := context.Background()
-	var telegramkey string = env_load()
 
-	app, err := NewApp(ctx, telegramkey)
+	conf, err := config.GetConfig()
+	if err != nil {
+		panic(err)
+	}
+	expvar.Publish("config", conf)
+
+	app, err := NewApp(ctx, conf)
 	if err != nil {
 		panic(err)
 	}
 
-	app.Run(ctx, telegramkey)
-}
-
-func env_load() string {
-	err := godotenv.Load()
-	var telegramkey string
-
-	if err != nil {
-
-		telegramkey = os.Getenv("TELEAGRAMBOT_KEY")
-		port := os.Getenv("PORT")
-		http.HandleFunc("/", MainHandler)
-		go http.ListenAndServe(":"+port, nil)
-
-	} else {
-
-		myEnv, err := godotenv.Read()
-		if err != nil {
-			log.Panic(err)
-		}
-		telegramkey = myEnv["TELEAGRAMBOT_KEY"]
-
-	}
-	return telegramkey
-}
-func MainHandler(resp http.ResponseWriter, _ *http.Request) {
-	resp.Write([]byte("Hi there! I'm DndSpellsBot!"))
+	app.Run(ctx)
 }
--- internal/config/env.go
+++ internal/config/env.go
@@ -1,7 +1,47 @@
 package config
 
+import (
+	"encoding/json"
+
+	secrets "github.com/ijustfool/docker-secrets"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/mitchellh/mapstructure"
+)
+
 type Config struct {
 	Port           string `envconfig:"PORT" default:"5001"`
 	TelegramApiKey string `envconfig:"TELEAGRAMBOT_KEY" default:""`
 	GiphiKey       string `envconfig:"GIPHY_KEY" default:""`
+}
+
+// GetConfig - создаёт конфиг на основе переменных окружения  префиксом WORKERBOT_ и docker secrets.
+func GetConfig() (*Config, error) {
+	cfg := &Config{}
+	err := envconfig.Process("TELEGRAM_BOT", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = updateFromDockerSecrets(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func updateFromDockerSecrets(cfg *Config) error {
+	dockerSecrets, err := secrets.NewDockerSecrets("")
+	if err != nil {
+		return nil
+	}
+	return mapstructure.Decode(dockerSecrets.GetAll(), cfg)
+}
+
+// String - генерирует строковое представление конфигурации.
+func (c *Config) String() string {
+	data, err := json.Marshal(c)
+	if err != nil {
+		return ""
+	}
+	return string(data)
 }
